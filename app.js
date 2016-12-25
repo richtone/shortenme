66d061450f6ec4e659e3378dde9e0107c96bdc8c
Richard <richard.turza@gmail.com>
2016-12-25 00:00:53 +0000

    adding new urls to mongoDB

--- app.js
+++ app.js
@@ -1,20 +1,64 @@
 "use strict";
 
 var express = require("express");
+var shortHash = require('short-hash');
 var mongodb = require("mongodb").MongoClient;
 
 var server_port = process.env.PORT || 8080;
 var dbURL = process.env.MONGOLAB_URI;
+var newReg = /^\/new\/[a-z0-9-]+:\/\/\S+\.\S+/i;
 
 console.log(dbURL);
 
-mongodb.connect(dbURL, (err, db) => {
-    if (err) {
-    console.log("Unable to connect to the"+dbURL+" Error:", err);
-    console.log(dbURL);
-  } else {
-    console.log("Connection established to", dbURL);
-  }
+var insertNewURL = function (db, response, callback) {
     
-    db.close();
+    db
+    .collection("shorts")
+    .insert(response, (err, data) => {
+        if (err) throw err;
+        console.log(response," zapísané do DB");
+        callback(data);
+    });
+    
+};
+
+express()
+.get("/", (req, res) => {
+    
+    res.end("ShortenMe app");
+    
 })
+.get(newReg, (req, res) => {
+    
+    let original_url = req.url.substring(5);
+    let short_url = shortHash(original_url);
+    let response = { "original_url":original_url, "short_url": short_url};
+    
+    mongodb.connect(dbURL, (err, db) => {
+        if (err) {
+        console.log("Unable to connect to the mongoDB. Error:", err);
+      } else {
+        console.log("Connection established to mongoDB");
+      }
+        
+        db
+        .collection("shorts")
+        .count({ "short_url" : {$eq : short_url} }, (err, count) => {
+            if (err) throw err;
+            
+            console.log("count = ", count);
+            if (count == 0 )  {
+                insertNewURL(db, response, () => {
+                    db.close();
+                });
+            } else {
+                console.log(response," nebola zapísaná do DB");
+                db.close();
+            }
+        });
+    });
+    
+    res.end(JSON.stringify(response));
+    
+})
+.listen(server_port);
