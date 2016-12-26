6898e2f73496ab414f0e3bac00ebc73c5531377d
Richard <richard.turza@gmail.com>
2016-12-26 00:23:16 +0000

    it's working! yay!

--- app.js
+++ app.js
@@ -3,24 +3,48 @@
 var express = require("express");
 var shortHash = require('short-hash');
 var mongodb = require("mongodb").MongoClient;
+var appDB = null;
+var shortsCollection = null;
 
 var server_port = process.env.PORT || 8080;
 var dbURL = process.env.MONGOLAB_URI;
-var newReg = /^\/new\/[a-z0-9-]+:\/\/\S+\.\S+/i;
+var newRegEx = /^\/new\/[a-z0-9-]+:\/\/\S+\.\S+/i;
+var redirectRegEx = /^\/[a-z0-9]{8}$/; // reťazec o 8 znakoch
+var errorJSON = {"error":"Wrong url format, make sure you have a valid protocol and real site."}; 
 
-console.log(dbURL);
-
-var insertNewURL = function (db, response, callback) {
+mongodb.connect(dbURL, (err, db) => {
     
-    db
-    .collection("shorts")
+    if (err) {
+    console.log("Unable to connect to the mongoDB. Error:", err);
+    } else {
+    console.log("Connection established to mongoDB");
+    }
+    appDB = db;
+    shortsCollection = db.collection("shorts");
+});
+
+function insertNewURL (response, callback) {
+    
+    shortsCollection
     .insert(response, (err, data) => {
         if (err) throw err;
         console.log(response," zapísané do DB");
         callback(data);
     });
-    
-};
+}
+
+function getOrigURL (hash, res, callback) {
+    shortsCollection
+    .find(
+        { "short_url" : hash },
+        { "original_url" : 1 }
+    )
+    .toArray((err, data) => {
+        if (err) throw err;
+        console.log(data);
+        callback(data);
+    });
+}
 
 express()
 .get("/", (req, res) => {
@@ -28,7 +52,7 @@ express()
     res.end("ShortenMe app");
     
 })
-.get(newReg, (req, res) => {
+.get(newRegEx, (req, res) => {
     
     let original_url = req.url.substring(5);
     let hash = shortHash(original_url);
@@ -40,31 +64,48 @@ express()
     let responseDoc = { "original_url":original_url, "short_url": hash };
     let response = { "original_url":original_url, "short_url": short_url };
     
-    mongodb.connect(dbURL, (err, db) => {
-        if (err) {
-        console.log("Unable to connect to the mongoDB. Error:", err);
-      } else {
-        console.log("Connection established to mongoDB");
-      }
+    shortsCollection
+    .count({ "short_url" : hash }, (err, count) => {
         
-        db
-        .collection("shorts")
-        .count({ "short_url" : {$eq : hash} }, (err, count) => {
-            if (err) throw err;
-            
-            console.log("count = ", count);
-            if (count == 0 )  {
-                insertNewURL(db, responseDoc, () => {
-                    db.close();
-                });
-            } else {
-                console.log(responseDoc," nebola zapísaná do DB");
-                db.close();
-            }
-        });
+        if (err) throw err;
+        
+        console.log("count = ", count);
+        if (count == 0 )  {
+            insertNewURL(responseDoc, () => {
+                appDB.close();
+            });
+        } else {
+            console.log(responseDoc," nebola zapísaná do DB");
+        }
     });
     
     res.end(JSON.stringify(response));
     
+})
+.get(redirectRegEx, (req, res) => {
+    
+    let hash = req.url.substring(1);
+    let redirectURL = null;
+    
+    shortsCollection
+    .count({ "short_url" : hash }, (err, count) => {
+        
+        if (err) throw err;
+        
+        console.log("count = ", count);
+        if (count == 1 )  {
+            redirectURL = getOrigURL(hash, res, data => {
+                console.log(data.original_url);
+                res.redirect(data[0].original_url);
+                appDB.close();
+            });
+            
+        } else {
+            console.log(hash," nebol nájdený");
+            res.end(JSON.stringify(errorJSON));
+        }
+    });
+    
+   
 })
 .listen(server_port);
